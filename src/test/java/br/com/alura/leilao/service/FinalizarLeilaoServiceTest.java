1aeb32a625b0999c264ae43a7c72d9464bf12d1f
mnishimori <mnishimori@yahoo.com.br>
2022-10-18 15:37:08 -0300

    Primeiro teste. Verificar a finalização de um leilão

---
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -0,0 +1,68 @@
+package br.com.alura.leilao.service;
+
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+class FinalizarLeilaoServiceTest {
+
+    private FinalizarLeilaoService service;
+
+    @Mock
+    private LeilaoDao leilaoDao;
+
+    @BeforeEach
+    public void beforEach(){
+        MockitoAnnotations.initMocks(this);
+        this.service = new FinalizarLeilaoService(leilaoDao);
+    }
+
+    @Test
+    void deveriaFinalizarUmLeilao(){
+        List<Leilao> leiloes = this.leiloes();
+
+        Mockito
+                .when(leilaoDao.buscarLeiloesExpirados())
+                .thenReturn(leiloes);
+
+        service.finalizarLeiloesExpirados();
+
+        Leilao leilao = leiloes.get(0);
+        Assertions.assertTrue(leilao.isFechado());
+        Assertions.assertEquals(new BigDecimal("900"), leilao.getLanceVencedor().getValor());
+        Mockito.verify(leilaoDao).salvar(leilao);
+    }
+
+
+    private List<Leilao> leiloes() {
+        List<Leilao> lista = new ArrayList<>();
+
+        Leilao leilao = new Leilao("Celular",
+                new BigDecimal("500"),
+                new Usuario("Fulano"));
+
+        Lance primeiro = new Lance(new Usuario("Beltrano"),
+                new BigDecimal("600"));
+        Lance segundo = new Lance(new Usuario("Ciclano"),
+                new BigDecimal("900"));
+
+        leilao.propoe(primeiro);
+        leilao.propoe(segundo);
+
+        lista.add(leilao);
+
+        return lista;
+    }
+
+}
